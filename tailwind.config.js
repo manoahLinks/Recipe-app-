de3ffbc7758c66c240d92d73a0e2b23993849bc0
manoahLinks <manoahluka@gmail.com>
2022-09-29 02:05:15 +0100

    made change to configuaration file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './components/**/*.{html,js}',
-  './pages/**/*.{html,js}',
-  './index.html'
-],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
